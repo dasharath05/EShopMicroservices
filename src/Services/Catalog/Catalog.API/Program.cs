5b30af8987c5f98ab75d9a9eedfc80502f29945a
Dasharath Vishwakarma <dasharath.vishwakarma@lns.maersk.com>
2024-05-31 10:08:30 +0530

    Catalog create product command

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
+
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
+var app = builder.Build();
+
+// Configure the HTTp request pipeline
+app.MapCarter();
+
+app.Run();
